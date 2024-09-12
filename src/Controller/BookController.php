171804b90b7621f10ebdfb9bdc9219f0ba4bee66
luljn <mbohlulajonathan4@gmail.com>
2024-09-13 00:34:38 +0200

    route to get a specific book by his id functional

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -9,14 +9,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/api/books', name: 'app_book', methods: ['GET'])]
 class BookController extends AbstractController
 {
-    #[Route('/api/books', name: 'app_book', methods: ['GET'])]
+    #[Route('', name: 'app_book', methods: ['GET'])]
     public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
         $bookList = $bookRepository->findAll();
         
         $jsonBookList = $serializer->serialize($bookList, 'json');
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{id}', name: 'app_book_detail', methods: ['GET'])]
+    public function getDetailBook(int $id, BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $book = $bookRepository->find($id);
+        if($book){
+
+            $jsonBook = $serializer->serialize($book, 'json');
+            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
