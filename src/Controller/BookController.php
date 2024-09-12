e893cd8d67e1cb731c9c040fa4fda9004d2d8841
luljn <mbohlulajonathan4@gmail.com>
2024-09-13 00:26:41 +0200

    route to get all the books functional

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
-    #[Route('/api/books', name: 'app_book')]
-    public function getAllBooks(): JsonResponse
+    #[Route('/api/books', name: 'app_book', methods: ['GET'])]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
-        ]);
+        $bookList = $bookRepository->findAll();
+        
+        $jsonBookList = $serializer->serialize($bookList, 'json');
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
 }
