ae4d362622753547eb77901e9f4ead7f635cd214
luljn <mbohlulajonathan4@gmail.com>
2024-09-14 00:23:46 +0200

    route to get a specific author by his id functional

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
 use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,5 +19,12 @@ class AuthorController extends AbstractController
         $authorList = $repository->findAll();
         $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getAuthors']);
         return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{id}', name: 'app_author_detail', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function getDetailBook(Author $author, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
     }
 }
