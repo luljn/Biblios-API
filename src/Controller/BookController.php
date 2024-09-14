6d59b0f398a9711ab6da502f56a2935bc6fc3295
luljn <mbohlulajonathan4@gmail.com>
2024-09-14 23:40:55 +0200

    route to update informations about a book added

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/books')]
@@ -52,6 +53,28 @@ class BookController extends AbstractController
     {
         $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
         return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/edit/{id}', name: 'app_book_update', requirements: ['id' => '\d+'], methods: ['PUT'])]
+    public function updateBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $manager,
+                                Book $currentBook, UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository): JsonResponse
+    {
+        $updatedBook = $serializer->deserialize($request->getContent(), Book::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentBook]);
+        
+        $content = $request->toArray();
+        $idAuthor = $content['idAuthor'] ?? -1;
+
+        $updatedBook->setAuthor($authorRepository->find($idAuthor));
+
+        $manager->persist($updatedBook);
+        $manager->flush();
+
+        // $jsonBook = $serializer->serialize($updatedBook, 'json', ['groups' => 'getBooks']);
+
+        // $location = $urlGenerator->generate('app_book_detail', ['id' => $updatedBook->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        // return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/delete/{id}', name: 'app_book_delete', requirements: ['id' => '\d+'], methods: ['DELETE'])]
