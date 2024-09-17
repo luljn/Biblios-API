fe15a5d7057f9a210e2c31fdd73db194dd7c6b6c
luljn <mbohlulajonathan4@gmail.com>
2024-09-17 03:41:50 +0200

    Database filled with users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,34 @@ namespace App\DataFixtures;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
+        $user = new User();
+        $user->setEmail("user@bookapi.com");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "azertyuiop1234"));
+        $manager->persist($user);
+
+        $userAdmin = new User();
+        $userAdmin->setEmail("admin@bookapi.com");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "azertyuiop1234"));
+        $manager->persist($userAdmin);
+
+        $listAuthors = [];
         for ($i=0; $i < 20; $i++) { 
             
             $author = new Author();
