3ec96dbf0d9391bff8b25636053f015e9a63f0fa
luljn <mbohlulajonathan4@gmail.com>
2024-09-18 04:32:00 +0200

    Simple pagination implemented to get information about the books

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -21,9 +21,12 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class BookController extends AbstractController
 {
     #[Route('', name: 'app_book', methods: ['GET'])]
-    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        $bookList = $bookRepository->findAll();
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+
+        $bookList = $bookRepository->findAllWithPagination($page, $limit);
         
         $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
